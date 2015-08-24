0e5d5c594a2c6981df987f11c0d8435315ba1ae6
stevesmith@epesent.com <stevesmith@epesent.com>
2015-08-24 11:52:39 -0500

    New

---
+++ trAddClass1x1.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Fytmness Made Easy - Add Class</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="css/tr.css" rel="stylesheet" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+    <div id="topbanner">
+        <?php include_once 'includes/inc.trainer.banner.php' ?>
+    </div><!--end topbanner-->
+
+
+
+    <div id="botbanner" class="botfix">
+        <?php include_once 'includes/inc.botbanner.php'; ?>
+    </div><!--end botbanner-->
+</body>
+</html>
