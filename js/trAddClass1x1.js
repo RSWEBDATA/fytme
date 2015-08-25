d58612a7242be4e1bc86ce2e951804450ed7e8f3
stevesmith@epesent.com <stevesmith@epesent.com>
2015-08-25 10:01:05 -0500

    20150825 am

---
+++ js/trAddClass1x1.js
@@ -0,0 +1,17 @@
+//sets amount input in proper currency format.
+(function($) {
+    $.fn.currencyFormat = function() {
+        this.each( function( i ) {
+            $(this).change( function( e ){
+                if( isNaN( parseFloat( this.value ) ) ) return;
+                this.value = parseFloat(this.value).toFixed(2);
+            });
+        });
+        return this; //for chaining
+    }
+})( jQuery );
+
+
+$( function() {
+    $('.currency').currencyFormat();
+});
