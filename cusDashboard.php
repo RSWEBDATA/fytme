1496bdb1f5a3f36be94ee76ac39789025a877788
stevesmith@epesent.com <stevesmith@epesent.com>
2015-09-02 10:07:59 -0500

    20150902 10:07

--- cusDashboard.php
+++ cusDashboard.php
@@ -8,10 +8,17 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>FytMe</title>
+    <title>FytMe Customer Dashboard</title>
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Petit+Formal+Script' rel='stylesheet' type='text/css'>
-    <link href="css/main.css" rel="stylesheet" type="text/css" />
+    <link href="css/cus.css" rel="stylesheet" type="text/css" />
+
+    <style type="text/css">
+        body {
+            background: url("images/cusBkgnd.jpg") no-repeat fixed 50% 50%;
+            background-size: cover;
+        }
+    </style>
 </head>
 <body>
 
