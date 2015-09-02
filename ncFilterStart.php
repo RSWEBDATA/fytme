a2b401d0bf50b8e89ba7f017943da268d679ccc6
stevesmith@epesent.com <stevesmith@epesent.com>
2015-09-02 10:08:51 -0500

    20150902 10:08

---
+++ ncFilterStart.php
@@ -0,0 +1,33 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>FytMe Choose a session</title>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Petit+Formal+Script' rel='stylesheet' type='text/css'>
+    <link href="css/cus.css" rel="stylesheet" type="text/css" />
+
+    <style type="text/css">
+        body {
+            background: url("images/cusBkgnd.jpg") no-repeat fixed 50% 50%;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+<div id="topbanner">
+    <?php include_once 'includes/inc.customer.banner.php' ?>
+</div><!--end topbanner-->
+
+<div id="botbanner" class="botfix">
+    <?php include_once 'includes/inc.botbanner.php'; ?>
+</div><!--end botbanner-->
+
+</body>
+</html>
