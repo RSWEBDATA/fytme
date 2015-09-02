8a157ce2322545c1923d307e7f6e558e63962726
stevesmith@epesent.com <stevesmith@epesent.com>
2015-09-02 10:01:18 -0500

    20150902 10:01

--- includes/inc.customer.banner.php
+++ includes/inc.customer.banner.php
@@ -8,4 +8,8 @@
         <a href="index.php"><img src="images/fytmelogosplashBIG.png" alt="Fytme Logo"></a>
     </div><!--end logo-->
 </div><!--end tbleft-->
-<div id="tbright"></div><!--end tbleft-->
+<div id="tbright">
+    <ul class="tbnav">
+        <li><a href="../cusDashboard.php" class="tblink">DASHBOARD</a></li>
+    </ul>
+</div><!--end tbright-->
